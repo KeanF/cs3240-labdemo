2c12675e3e5c36787e20ef0a5578a458dd49b9ca
Kean Finucane <kf9zy@virginia.edu>
2017-02-18 19:54:26 -0500

    Initial commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+"""
+Kean Finucane (kf9zy)
+HW3 - Dictionaries, Test Cases, and Unit-Testing
+CS 3240 - Smith
+16 February 2017
+"""
+__author__ = 'Kean Finucane'
+
+if __name__ == '__main__':
+  print('hello')
