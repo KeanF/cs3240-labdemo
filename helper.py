270d644cc1074ac09e00a86821c991732fbb683d
Kean Finucane <kf9zy@virginia.edu>
2017-02-20 17:08:52 -0500

    Refactor methods for greater modularity

--- helper.py
+++ helper.py
@@ -2,11 +2,14 @@
 Kean Finucane (kf9zy)
 Lab 5 - Git and Github
 CS 3240 - Smith
-16 February 2017
+20 February 2017
 """
 __author__ = 'Kean Finucane'
 
-from helper import greeting
+
+def greeting(msg):
+    print(msg)
+
 
 if __name__ == '__main__':
-    greeting('hello')
+    pass
